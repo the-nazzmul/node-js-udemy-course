928571f88bbe5abd6720095805cb629bfbfe701b
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-31 21:21:13 +0600

    end of day 1

--- 1.javascript-bonus/part1/changes.js
+++ 1.javascript-bonus/part1/changes.js
@@ -1,1 +1,11 @@
 let gameName = "pubg";
+
+gameName = "NFS";
+
+console.log(gameName);
+
+const username = "the-nazzmul";
+
+username = "hudai-nazzmul";
+
+console.log(username);
---
+++ 1.javascript-bonus/part1/operations.js
@@ -0,0 +1,15 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let mult = 3 * 5;
+let divi = 8 / 2;
+let remainder = 9 % 2;
+let expo = 2 ** 3;
+
+
+let myScore = 110;
+myScore ++
