bc2079f1de77edeee03574126e2b9e2f7ec2feb4
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-04-02 23:14:46 +0600

    end of day 2

---
+++ 1.javascript-bonus/part3/loop.js
@@ -0,0 +1,39 @@
+let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+  sum += i;
+  i++;
+}
+
+// console.log(sum);
+
+let countdown = [];
+let j = 5;
+
+while (j >= 1) {
+  countdown.push(j);
+  j--;
+}
+
+// console.log(countdown);
+
+// let teaCollection = [];
+// let tea;
+
+// do {
+//   tea = prompt(`Enter your favorite tea. (type "stop" to finish)`);
+//   if (tea !== "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea !== "stop");
+
+let total = 0;
+let k = 1;
+
+do {
+  total += k;
+  k++;
+} while (k <= 3);
+
+console.log(total);
